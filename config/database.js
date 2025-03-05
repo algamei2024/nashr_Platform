77d09fb8faae9893ad4d9d636dd4f8ed833dcf00
Mohammed hamoud <151775931+algamei2024@users.noreply.github.com>
2025-03-05 23:57:48 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://malgamei2024:<w53nZWfVzzIoxbhL>@nashr.j42io.mongodb.net/<nashr>?retryWrites=true&w=majority&appName=Nashr').then((con) => {
+mongoose.connect('mongodb+srv://malgamei2024:w53nZWfVzzIoxbhL@nashr.j42io.mongodb.net/<nashr>?retryWrites=true&w=majority&appName=Nashr').then((con) => {
     console.log('successfully');
 }).catch((err) => {
     console.log('error');
